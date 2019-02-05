627e49fb762e75985c8d4e014ff2874635539540
AnhVoMBP15 <anhvouw@gmail.com>
2019-02-05 17:47:04 -0500

    add statusbar component, fetch schedule on startup, fix flickering

--- src/components/page.js
+++ src/components/page.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import { View, StyleSheet, } from 'react-native'
+import { View, StyleSheet, StatusBar } from 'react-native'
 import { SafeAreaView } from 'react-navigation'
 import TopBar from './top_bar'
 
 
 const Page = ({ children, pageName }) => {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
       <View style={styles.container}>
+        <StatusBar hidden={false} />
         <TopBar page={pageName} />
         {children}
       </View>
-      
+
     </SafeAreaView>
   )
 }
