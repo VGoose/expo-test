0292d9a4c775a641292439ffab46c651b9d0cf91
AnhVoMBP15 <anhvouw@gmail.com>
2019-02-28 11:32:54 -0500

    fix info layout and allow currentPage setting

--- src/components/transit_module.js
+++ src/components/transit_module.js
@@ -6,8 +6,16 @@ import CountdownClock from './countdown_clock'
 import { padding, fonts, colors, margin } from '../styles/base'
 
 
-class TransitModule extends React.Component {
-  _renderItem = (item) => {
+const TransitModule = ({
+  isNearby: showNearbyStationsFirst,
+  scheduleLastUpdated,
+  scheduleData = {},
+  isFetching,
+  favoriteStations,
+  nearbyStations,
+  toggleFavorite,
+  fetchSchedule }) => {
+  const _renderItem = (item) => {
     const { data, isEmpty, fetch, emptyText, header } = item
     return <Slide
       data={data}
@@ -17,98 +25,86 @@ class TransitModule extends React.Component {
       header={header}
     />
   }
-  render() {
-    const {
-      isNearby: showNearbyStationsFirst,
-      scheduleLastUpdated,
-      scheduleData = {},
-      isFetching,
-      favoriteStations,
-      nearbyStations,
-      toggleFavorite,
-      fetchSchedule } = this.props
-    const isEmpty = Object.entries(scheduleData).length === 0 && scheduleData.constructor === Object
-
-    //make countdown clocks from schedule scheduleData
-    let northSchedule, southSchedule
-    const _isFav = (id) => {
-      return favoriteStations.some((station) => id === station.stop_id)
-    }
-    //TODO add data freshness indicator 
-    const favoriteStationsCountdowns = favoriteStations
-      .map(station => {
-        //keys in schedules are stop_id + N/S
-        northSchedule = station.stop_id + 'N' in scheduleData ? scheduleData[station.stop_id + 'N'] : [];
-        southSchedule = station.stop_id + 'S' in scheduleData ? scheduleData[station.stop_id + 'S'] : [];
-        return <CountdownClock
-          defaultOpen
-          key={station.stop_id}
-          id={station.stop_id}
-          isFetching={isFetching}
-          toggleFavorite={toggleFavorite}
-          schedules={[
-            ...southSchedule,
-            ...northSchedule
-          ]}
-          isFav
-          name={station.stop_name}
-        />
-      })
-    const nearbyStationCountdowns = nearbyStations
-      .map((station, index) => {
-        northSchedule = station.stop_id + 'N' in scheduleData ? scheduleData[station.stop_id + 'N'] : [];
-        southSchedule = station.stop_id + 'S' in scheduleData ? scheduleData[station.stop_id + 'S'] : [];
-        return <CountdownClock
-          defaultOpen={index == 0 ? true : false}
-          isNearby
-          key={station.stop_id}
-          id={station.stop_id}
-          toggleFavorite={toggleFavorite}
-          isFetching={isFetching}
-          schedules={[
-            ...northSchedule,
-            ...southSchedule
-          ]}
-          isFav={_isFav(station.stop_id)}
-          name={station.stop_name}
-        />
-      })
-    const isNearbyEmpty = nearbyStationCountdowns.length === 0
-    const isFavoriteEmpty = favoriteStationsCountdowns.length === 0
-    const nearbyEmptyText = "There are no stations nearby."
-    const favoriteEmptyText = "You haven't added any stations to your favorites.  \
+  //make countdown clocks from schedule scheduleData
+  let northSchedule, southSchedule
+  const _isFav = (id) => {
+    return favoriteStations.some((station) => id === station.stop_id)
+  }
+  //TODO add data freshness indicator 
+  const favoriteStationsCountdowns = favoriteStations
+    .map(station => {
+      //keys in schedules are stop_id + N/S
+      northSchedule = station.stop_id + 'N' in scheduleData ? scheduleData[station.stop_id + 'N'] : [];
+      southSchedule = station.stop_id + 'S' in scheduleData ? scheduleData[station.stop_id + 'S'] : [];
+      return <CountdownClock
+        defaultOpen
+        key={station.stop_id}
+        id={station.stop_id}
+        isFetching={isFetching}
+        toggleFavorite={toggleFavorite}
+        schedules={[
+          ...southSchedule,
+          ...northSchedule
+        ]}
+        isFav
+        name={station.stop_name}
+      />
+    })
+  const nearbyStationCountdowns = nearbyStations
+    .map((station, index) => {
+      northSchedule = station.stop_id + 'N' in scheduleData ? scheduleData[station.stop_id + 'N'] : [];
+      southSchedule = station.stop_id + 'S' in scheduleData ? scheduleData[station.stop_id + 'S'] : [];
+      return <CountdownClock
+        defaultOpen={index == 0 ? true : false}
+        isNearby
+        key={station.stop_id}
+        id={station.stop_id}
+        toggleFavorite={toggleFavorite}
+        isFetching={isFetching}
+        schedules={[
+          ...northSchedule,
+          ...southSchedule
+        ]}
+        isFav={_isFav(station.stop_id)}
+        name={station.stop_name}
+      />
+    })
+  const isNearbyEmpty = nearbyStationCountdowns.length === 0
+  const isFavoriteEmpty = favoriteStationsCountdowns.length === 0
+  const nearbyEmptyText = "There are no stations nearby."
+  const favoriteEmptyText = "You haven't added any stations to your favorites.  \
   Press on the station pins to add stations to your list."
 
-    const _data = [
-      {
-        data: nearbyStationCountdowns,
-        isEmpty: isNearbyEmpty,
-        fetch: fetchSchedule,
-        emptyText: nearbyEmptyText,
-        header: "nearby stations",
-      },
-      {
-        data: favoriteStationsCountdowns,
-        isEmpty: isFavoriteEmpty,
-        fetch: fetchSchedule,
-        emptyText: favoriteEmptyText,
-        header: "favorite stations",
-      }
-    ]
+  const _data = [
+    {
+      data: nearbyStationCountdowns,
+      isEmpty: isNearbyEmpty,
+      fetch: fetchSchedule,
+      emptyText: nearbyEmptyText,
+      header: "nearby stations",
+    },
+    {
+      data: favoriteStationsCountdowns,
+      isEmpty: isFavoriteEmpty,
+      fetch: fetchSchedule,
+      emptyText: favoriteEmptyText,
+      header: "favorite stations",
+    }
+  ]
 
-    return <Carousel 
-          style={styles.container}
-          autoplay={false}
-          pageInfo
-          isLooped={false}
-          pageInfoBottomContainerStyle={{ top: 5, right: 5, bottom: undefined, left: undefined }}
-          pageInfoBackgroundColor='transparent'
-        >
-          {this._renderItem(_data[0])}
-          {this._renderItem(_data[1])}
-        </Carousel>
-
-  }
+  return <Carousel
+    style={styles.container}
+    autoplay={false}
+    pageInfo
+    currentPage={showNearbyStationsFirst ? 0 : 1}
+    isLooped={false}
+    pageInfoTextStyle={{fontSize: fonts.md}}
+    pageInfoBottomContainerStyle={{ top: 5, right: -10, bottom: undefined, left: undefined }}
+    pageInfoBackgroundColor='transparent'
+  >
+    {_renderItem(_data[0])}
+    {_renderItem(_data[1])}
+  </Carousel>
 }
 const Slide = ({ header, isEmpty, fetch, emptyText, data }) => {
   return <View style={styles.swiperSlideContainer}>
